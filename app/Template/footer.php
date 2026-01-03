00f5cd18f01f1f7ff6cb866d0e5e751a81eb77ce
Gemah Ghozali <gemahghozali@gmail.com>
2026-01-03 08:12:52 +0800

    Feature (Template) : Add footer component

---
+++ app/Template/footer.php
@@ -0,0 +1,89 @@
+<!-- Footer -->
+<footer class="container-xxl p-0 bg-dark rounded-top-5 text-white">
+   <div class="row g-0 p-4 p-sm-5 gap-5 gap-lg-4">
+
+      <div class="col-12 col-lg-4">
+         <div class="d-flex align-items-center gap-3">
+            <div class="bg-white d-flex flex-grow-0 justify-content-center align-items-center rounded-circle" style="width: 48px; height: 48px;">
+               <i class="text-black bi bi-bank2 fs-5"></i>
+            </div>
+            <div class="text-white">
+               <h5 class="fw-bold m-0">Koperasi</h5>
+               <p class="m-0">Teknologi Informasi</p>
+            </div>
+         </div>
+         <ul class="ps-0 m-0 mt-4 d-flex flex-column gap-2 list-unstyled">
+            <li class="d-flex gap-2 align-items-start">
+               <i class="bi bi-geo-alt-fill"></i>
+               <span>Samarinda, Kalimantan Timur, Indonesia</span>
+            </li>
+            <li class="d-flex gap-2 align-items-start">
+               <i class="bi bi-telephone-fill"></i>
+               <span>+6212-3456-7890</span>
+            </li>
+            <li class="d-flex gap-2 align-items-start">
+               <i class="bi bi-envelope-fill"></i>
+               <span>koperasi.ti@gmail.com</span>
+            </li>
+         </ul>
+      </div>
+
+      <div class="col-12 col-sm-6 col-lg">
+         <h5 class="fw-bold mb-3">Quick Links</h5>
+         <ul class="ps-3 m-0 d-flex flex-column gap-2">
+            <li>
+               <a href="#" class="text-white">Beranda</a>
+            </li>
+            <li>
+               <a href="#" class="text-white">Tentang Kami</a>
+            </li>
+            <li>
+               <a href="#" class="text-white">Galeri</a>
+            </li>
+            <li>
+               <a href="#" class="text-white">Artikel</a>
+            </li>
+         </ul>
+      </div>
+
+      <div class="col-12 col-sm-4 col-lg">
+         <h5 class="fw-bold mb-3">Support</h5>
+         <ul class="ps-3 m-0 d-flex flex-column gap-2">
+            <li>
+               <a href="#" class="text-white">Privacy Policy</a>
+            </li>
+            <li>
+               <a href="#" class="text-white">FAQs</a>
+            </li>
+            <li>
+               <a href="#" class="text-white">Terms & Condition</a>
+            </li>
+         </ul>
+      </div>
+
+      <div class="col-12 col-lg-3">
+         <h5 class="fw-bold mb-3">Newsletter</h5>
+         <div class="d-flex gap-2 bg-white p-2 rounded-3">
+            <input class="form-control border-0 shadow-none" type="text" name="email" placeholder="Alamat Email Anda">
+            <div class="btn btn-dark">Subscribe</div>
+         </div>
+         <p class="my-3">Dapatkan informasi terbaru dari kami!</p>
+         <div class="d-flex gap-2">
+            <a href="#" class="rounded-circle d-flex justify-content-center align-items-center border border-white text-white" style="width: 42px; height: 42px;">
+               <i class="bi bi-instagram"></i>
+            </a>
+            <a href="#" class="rounded-circle d-flex justify-content-center align-items-center border border-white text-white" style="width: 42px; height: 42px;">
+               <i class="bi bi-facebook"></i>
+            </a>
+            <a href="#" class="rounded-circle d-flex justify-content-center align-items-center border border-white text-white" style="width: 42px; height: 42px;">
+               <i class="bi bi-tiktok"></i>
+            </a>
+            <a href="#" class="rounded-circle d-flex justify-content-center align-items-center border border-white text-white" style="width: 42px; height: 42px;">
+               <i class="bi bi-youtube"></i>
+            </a>
+         </div>
+      </div>
+   </div>
+   <hr class="border border-white m-0">
+   <div class="d-flex justify-content-center text-white p-4 text-center">&copy; 2025 Gemah Ghozali - All Rights Reserved</div>
+</footer>
