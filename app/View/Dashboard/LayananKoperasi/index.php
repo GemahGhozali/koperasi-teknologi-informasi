8643aaecdaa4400bdf22bcb18b5905356cb4d6e6
Gemah Ghozali <gemahghozali@gmail.com>
2026-01-03 08:29:37 +0800

    Feature (View) : Add dashboard layanan koperasi page

---
+++ app/View/Dashboard/LayananKoperasi/index.php
@@ -0,0 +1,35 @@
+<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="py-2 mb-4">
+   <ol class="breadcrumb mb-0">
+      <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Layanan Koperasi</li>
+   </ol>
+</nav>
+
+<header class="mb-4">
+   <h3 class="mb-2 fw-bold">Layanan Koperasi</h3>
+   <p class="m-0 text-body-secondary">Kelola konten gambar, video dan berita website koperasi disini</p>
+</header>
+
+<div class="row g-0 gap-4">
+   <a href="/dashboard/layanan-koperasi/pegadaian" class="col border border-2 border-black rounded-4 p-3 drop-shadow-md">
+      <div class="bg-dark rounded-2 d-flex justify-content-center align-items-center mb-3" style="width: 40px; height: 40px;">
+         <i class="bi bi-piggy-bank-fill text-white fs-6"></i>
+      </div>
+      <h5 class="fw-bold mb-1 text-black">Layanan Pegadaian</h5>
+      <p class="mb-0 text-body-secondary">Gadaikan diri anda untuk usaha</p>
+   </a>
+   <a href="/dashboard/layanan-koperasi/peminjaman" class="col border border-2 border-black rounded-4 p-3 drop-shadow-md">
+      <div class="bg-dark rounded-2 d-flex justify-content-center align-items-center mb-3" style="width: 40px; height: 40px;">
+         <i class="bi bi-briefcase-fill text-white fs-6"></i>
+      </div>
+      <h5 class="fw-bold mb-1 text-black">Layanan Peminjaman</h5>
+      <p class="mb-0 text-body-secondary">Kelola semua konten gambar anda</p>
+   </a>
+   <a href="/dashboard/layanan-koperasi/penitipan" class="col border border-2 border-black rounded-4 p-3 drop-shadow-md">
+      <div class="bg-dark rounded-2 d-flex justify-content-center align-items-center mb-3" style="width: 40px; height: 40px;">
+         <i class="bi bi-archive-fill text-white fs-6"></i>
+      </div>
+      <h5 class="fw-bold mb-1 text-black">Layanan Penitipan Barang</h5>
+      <p class="mb-0 text-body-secondary">Kelola semua konten berita anda</p>
+   </a>
+</div>
