5ad435ad83c6d9981506d0538032d74d048fb2b1
Gemah Ghozali <gemahghozali@gmail.com>
2026-01-03 08:23:50 +0800

    Feature (View) : Add dashboard manage news content page

---
+++ app/View/Dashboard/ManageContent/News/index.php
@@ -0,0 +1,19 @@
+<!-- Breadcrumbs -->
+<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="py-2 mb-4">
+   <ol class="breadcrumb mb-0">
+      <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
+      <li class="breadcrumb-item"><a href="/dashboard/manage-content">Manajemen Konten</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Berita</li>
+   </ol>
+</nav>
+
+<!-- Header -->
+<header class="d-flex justify-content-between align-items-end mb-4">
+   <div>
+      <h3 class="mb-2 fw-bold">Konten Berita</h3>
+      <p class="m-0 text-body-secondary">Kelola semua data berita disini</p>
+   </div>
+   <button class="btn btn-dark">
+      <span class="d-none d-sm-inline">Tambah Berita</span>
+      <i class="bi bi-plus-lg"></i></button>
+</header>
