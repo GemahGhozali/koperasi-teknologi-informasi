77138c0227c69fd5fb6192956a075c28b4468645
Gemah Ghozali <gemahghozali@gmail.com>
2026-01-03 08:17:31 +0800

    Feature (View) : Add dashboard overview page

---
+++ app/View/Dashboard/Overview/index.php
@@ -0,0 +1,5 @@
+<!-- Header -->
+<header class="mb-4">
+   <h3 class="mb-2 fw-bold">Selamat Datang di Halaman Dashboard</h3>
+   <p class="m-0 text-body-secondary">Lihat semua ringkasan aktivitas terkini disini</p>
+</header>
