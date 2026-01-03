4fc34f0c8d567e89a7553818cc4d5d5cad6a4c28
Gemah Ghozali <gemahghozali@gmail.com>
2026-01-03 08:21:04 +0800

    Feature (View) : Add dashboard manage content page

---
+++ app/View/Dashboard/ManageContent/index.php
@@ -0,0 +1,35 @@
+<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="py-2 mb-4">
+   <ol class="breadcrumb mb-0">
+      <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Manajemen Konten</li>
+   </ol>
+</nav>
+
+<header class="mb-4">
+   <h3 class="mb-2 fw-bold">Manajemen Konten</h3>
+   <p class="m-0 text-body-secondary">Kelola konten gambar, video dan berita website koperasi disini</p>
+</header>
+
+<div class="row g-0 gap-3 gap-sm-4">
+   <a href="/dashboard/manage-content/video" class="col-12 col-md border border-2 border-black rounded-4 p-3 drop-shadow-md">
+      <div class="bg-dark rounded-2 d-flex justify-content-center align-items-center mb-3" style="width: 40px; height: 40px;">
+         <i class="bi bi-youtube text-white fs-6"></i>
+      </div>
+      <h5 class="fw-bold mb-1 text-black">Konten Video</h5>
+      <p class="mb-0 text-body-secondary">Kelola semua konten video anda</p>
+   </a>
+   <a href="/dashboard/manage-content/image" class="col-12 col-md border border-2 border-black rounded-4 p-3 drop-shadow-md">
+      <div class="bg-dark rounded-2 d-flex justify-content-center align-items-center mb-3" style="width: 40px; height: 40px;">
+         <i class="bi bi-image text-white fs-6"></i>
+      </div>
+      <h5 class="fw-bold mb-1 text-black">Konten Gambar</h5>
+      <p class="mb-0 text-body-secondary">Kelola semua konten gambar anda</p>
+   </a>
+   <a href="/dashboard/manage-content/news" class="col-12 col-md border border-2 border-black rounded-4 p-3 drop-shadow-md">
+      <div class="bg-dark rounded-2 d-flex justify-content-center align-items-center mb-3" style="width: 40px; height: 40px;">
+         <i class="bi bi-megaphone-fill text-white fs-6"></i>
+      </div>
+      <h5 class="fw-bold mb-1 text-black">Konten Berita</h5>
+      <p class="mb-0 text-body-secondary">Kelola semua konten berita anda</p>
+   </a>
+</div>
